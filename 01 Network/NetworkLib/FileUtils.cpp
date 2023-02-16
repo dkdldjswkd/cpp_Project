eee762191aecd3706fa6b894f546c7de3da1ede8
dkdldjswkd <dkdldjswkd@naver.com>
2023-02-17 06:19:14 +0900

    파서 구현 중

--- 01 Network/NetworkLib/FileUtils.cpp
+++ 01 Network/NetworkLib/FileUtils.cpp
@@ -36,3 +36,39 @@ void LoadFile(const char* fileName, char** dst) {
 //
 //	fclose(fp);
 //}
+
+char* FindSubstr(const char* str, const char* sub, const char* exclude) {
+    const int strLen = std::strlen(str);
+    const int subLen = std::strlen(sub);
+    const int exLen = std::strlen(exclude);
+
+    for (int offset = 0; offset < strLen; offset++) {
+		// 주석 발견. 개행문자가 나올때까지 스킵
+        if (str[offset] == '/' && offset < strLen - 1 && str[offset + 1] == '/') {
+            while (offset < strLen && str[offset] != '\n') {
+                offset++;
+            }
+		}
+		// 비교할 필요 없는 문자들 건너뛰기
+		else if (str[offset] == ' ' || str[offset] == '\n' || str[offset] == '\t') {
+			continue;
+		}
+		// str 남은 문자열에 sub가 속할 수 없음. 함수 반환
+		else if (strLen < offset + subLen) {
+			return nullptr;
+		}
+		// str 남은 문자열에 exclude가 속할 수 있음. exclude 발견 시 함수 종료
+		else if (offset + exLen <= strLen) {
+			if (std::strncmp(str + offset, exclude, exLen) == 0) {
+				return nullptr;
+			}
+		}
+		else {
+			// 문자비교
+			if (std::strncmp(str + offset, sub, subLen) == 0) {
+				return (char*)&str[offset + subLen];
+			}
+		}
+	}
+	return nullptr;
+}
